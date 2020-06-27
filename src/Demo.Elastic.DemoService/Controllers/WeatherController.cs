d368e1c4f1901dc6dd28236897caa48ccf4bd3a1
Rodion Novoselov <r.novoselov@outlook.com>
2020-06-28 00:59:07 +0300

    Demo service.

---
+++ src/Demo.Elastic.DemoService/Controllers/WeatherController.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Elastic.DemoService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Elastic.DemoService.Controllers
+{
+    [ApiController]
+    [Route("api/weather")]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet("")]
+        public WeatherModel GetWeather() =>
+            new WeatherModel {
+                Temperature = 9,
+                Pressure = 1013,
+                Humidity = 78,
+                Wind = 7
+            };
+    }
+}
---
+++ src/Demo.Elastic.DemoService/Models/WeatherModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo.Elastic.DemoService.Models
+{
+    public class WeatherModel
+    {
+        public int Temperature { get; set; }
+
+        public int Pressure { get; set; }
+
+        public int Humidity { get; set; }
+
+        public int Wind { get; set; }
+    }
+}
